92b80b8be3a3d25e2cca47c3de27683a455b984c
Jul0P <180294787+Jul0P@users.noreply.github.com>
2025-04-04 21:03:53 +0200

    feat(filter): add visual integration for the filter component

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -15,6 +15,20 @@ class DashboardController extends AbstractController
         // Récupérer le terme de recherche depuis la requête
         $search = $request->query->get('search', '');
 
+        $options = [
+            ['value' => 'Rouen', 'label' => 'Rouen'],
+            ['value' => 'Friville', 'label' => 'Friville'],
+            ['value' => 'Mont-Saint-Aignan', 'label' => 'Mont-Saint-Aignan'],
+            ['value' => 'Dreux Cedex', 'label' => 'Dreux Cedex'],
+        ];
+
+        $facets = [
+            'Rouen' => 1,
+            'Friville' => 1,
+            'Mont-Saint-Aignan' => 1,
+            'Dreux Cedex' => 1,
+        ];
+
         // Si un terme de recherche est fourni, rechercher les entreprises correspondantes
         // Sinon, récupérer toutes les entreprises
         $entreprises = $search ? $entrepriseRepository->findByRaisonSociale($search) : $entrepriseRepository->findAll();
@@ -23,6 +37,9 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'entreprises' => $entreprises,
             'search' => $search,
+            'options' => $options,
+            'facets' => $facets,
+            'title' => 'Ville',
         ]);
     }
 }
